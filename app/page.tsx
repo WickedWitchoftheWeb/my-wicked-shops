b21702ab9a9e6eeb84cb8587de78da7297a98813
Wicked <wickedwitchsweb@gmail.com>
2026-04-03 14:21:08 -0500

    remove missing primary header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import Link from "next/link";
-import PrimaryHeader from "../components/PrimaryHeader";
 
 export default function HomePage() {
   return (
     <>
-      <PrimaryHeader />
 
       <main className="grimoire-wrap">
         <div className="grimoire-container">
